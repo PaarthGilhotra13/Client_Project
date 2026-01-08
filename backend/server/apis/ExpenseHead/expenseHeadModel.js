d97810e3ebe607bae01ab07de09bae37f54f5367
Priyanshu Garg <129214059+gargpriyanshu66@users.noreply.github.com>
2026-01-09 00:21:55 +0530

    hi

--- backend/server/apis/ExpenseHead/expenseHeadModel.js
+++ backend/server/apis/ExpenseHead/expenseHeadModel.js
@@ -7,8 +7,5 @@ const expenseHeadSchema=new mongoose.Schema({
     createdAt:{type:Date,default:Date.now()}
 })
 
-<<<<<<< HEAD
 module.exports=new mongoose.model("expenseHeadData",expenseHeadSchema)
-=======
-module.exports=new mongoose.model("expenseHead",expenseHeadSchema)
->>>>>>> 97338558a47ba0127299be0b5a0f27cb2ad40678
+
