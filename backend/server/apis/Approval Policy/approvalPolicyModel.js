a83ab3655fccdae219a7c4b026f3c5bd6b5451af
Priyanshu Garg <129214059+gargpriyanshu66@users.noreply.github.com>
2026-01-09 00:17:23 +0530

    hello

---
+++ backend/server/apis/Approval Policy/approvalPolicyModel.js
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose")
+
+const approvalPolicySchema=new mongoose.Schema({
+    minAmount:{type:Number,default:""},
+    maxAmount:{type:Number,default:""},
+    approvalLevels:[{type:String,default:""}],
+    status:{type:Boolean,default:true},
+    createdAt:{type:Date,default:Date.now()}
+})
+
+module.exports=new mongoose.model("Approval Policy",approvalPolicySchema)
